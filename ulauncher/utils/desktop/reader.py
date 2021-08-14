9b898f7fc48c5947a99fad9bf467000aee472f6e
Albin Larsson <mail@albinlarsson.com>
2021-08-14 17:09:47 +0200

    Show gnome-control-center panels directly in app

--- ulauncher/utils/desktop/reader.py
+++ ulauncher/utils/desktop/reader.py
@@ -59,8 +59,11 @@ def filter_app(app):
     :param Gio.DesktopAppInfo app:
     :returns: True if app can be added to the database
     """
+    # Make an exception for gnome-control-center, because all the very useful specific settings
+    # like "Keyboard", "Wi-Fi", "Sound" etc have NoDisplay=true
+    nodisplay = app and app.get_nodisplay() and not app.get_executable() == 'gnome-control-center'
     return app and app.get_string('Name') and app.get_string('Type') == 'Application' \
-        and app.get_show_in() and not app.get_nodisplay() and not app.get_is_hidden()
+        and app.get_show_in() and not nodisplay and not app.get_is_hidden()
 
 
 def read_desktop_file(file):
